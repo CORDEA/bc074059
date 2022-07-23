af6eac46d87393230d9bb037f56414bd467b3d5b
Yoshihiro Tanaka <contact@cordea.jp>
2022-07-23 11:55:33 +1000

    initial commit :)

---
+++ bin/mockito_mocktail_migration_helper.dart
@@ -0,0 +1,5 @@
+import 'package:mockito_mocktail_migration_helper/mockito_mocktail_migration_helper.dart' as mockito_mocktail_migration_helper;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mockito_mocktail_migration_helper.calculate()}!');
+}
---
+++ test/mockito_mocktail_migration_helper_test.dart
@@ -0,0 +1,8 @@
+import 'package:mockito_mocktail_migration_helper/mockito_mocktail_migration_helper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
